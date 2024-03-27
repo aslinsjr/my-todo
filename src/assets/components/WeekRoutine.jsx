dd6a548bdf3a949b08edb4c7e1bf6594ddd1b7aa
aslinsjr <aslinsjr@gmail.com>
2024-03-27 06:34:04 -0300

    WeekRoutine Component
    Mudanças na forma como exibir a tarefa completada

--- src/assets/components/WeekRoutine.jsx
+++ src/assets/components/WeekRoutine.jsx
@@ -29,43 +29,43 @@ const WeekRoutine = () => {
             </tr>
             <tr>
                 <th>Meditação / Leitura</th>
-                <td><i className="bi bi-check-lg meditation" style={monday.meditation ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg meditation" style={tuesday.meditation ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg meditation" style={wednesday.meditation ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg meditation" style={thursday.meditation ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg meditation" style={friday.meditation ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg meditation" style={saturday.meditation ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg meditation" style={sunday.meditation ? {display: "flex"} : {}}></i></td>
+                <td>{monday.meditation ? <i className="bi bi-check-lg meditation"></i> : <></> }</td>
+                <td>{tuesday.meditation ? <i className="bi bi-check-lg meditation"></i> : <></> }</td>
+                <td>{wednesday.meditation ? <i className="bi bi-check-lg meditation"></i> : <></> }</td>
+                <td>{thursday.meditation ? <i className="bi bi-check-lg meditation"></i> : <></> }</td>
+                <td>{friday.meditation ? <i className="bi bi-check-lg meditation"></i> : <></> }</td>
+                <td>{saturday.meditation ? <i className="bi bi-check-lg meditation"></i> : <></> }</td>
+                <td>{sunday.meditation ? <i className="bi bi-check-lg meditation"></i> : <></> }</td>
             </tr>
             <tr>
                 <th>Treino / Alongamento</th>
-                <td><i className="bi bi-check-lg workout" style={monday.workout ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg workout" style={tuesday.workout ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg workout" style={wednesday.workout ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg workout" style={thursday.workout ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg workout" style={friday.workout ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg workout" style={saturday.workout ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg workout" style={sunday.workout ? {display: "flex"} : {}}></i></td>
+                <td>{monday.workout ? <i className="bi bi-check-lg workout"></i> : <></> }</td>
+                <td>{tuesday.workout ? <i className="bi bi-check-lg workout"></i> : <></> }</td>
+                <td>{wednesday.workout ? <i className="bi bi-check-lg workout"></i> : <></> }</td>
+                <td>{thursday.workout ? <i className="bi bi-check-lg workout"></i> : <></> }</td>
+                <td>{friday.workout ? <i className="bi bi-check-lg workout"></i> : <></> }</td>
+                <td>{saturday.workout ? <i className="bi bi-check-lg workout"></i> : <></> }</td>
+                <td>{sunday.workout ? <i className="bi bi-check-lg workout"></i> : <></> }</td>
             </tr>
             <tr>
                 <th>Estudo / Prática</th>
-                <td><i className="bi bi-check-lg study" style={monday.study ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg study" style={tuesday.study ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg study" style={wednesday.study ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg study" style={thursday.study ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg study" style={friday.study ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg study" style={saturday.study ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg study" style={sunday.study ? {display: "flex"} : {}}></i></td>
+                <td>{monday.study ? <i className="bi bi-check-lg study"></i> : <></> }</td>
+                <td>{tuesday.study ? <i className="bi bi-check-lg study"></i> : <></> }</td>
+                <td>{wednesday.study ? <i className="bi bi-check-lg study"></i> : <></> }</td>
+                <td>{thursday.study ? <i className="bi bi-check-lg study"></i> : <></> }</td>
+                <td>{friday.study ? <i className="bi bi-check-lg study"></i> : <></> }</td>
+                <td>{saturday.study ? <i className="bi bi-check-lg study"></i> : <></> }</td>
+                <td>{sunday.study ? <i className="bi bi-check-lg study"></i> : <></> }</td>
             </tr>
             <tr>
                 <th>Currículo / LinkedIn</th>
-                <td><i className="bi bi-check-lg linkedin" style={monday.linkedin ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg linkedin" style={tuesday.linkedin ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg linkedin" style={wednesday.linkedin ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg linkedin" style={thursday.linkedin ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg linkedin" style={friday.linkedin ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg linkedin" style={saturday.linkedin ? {display: "flex"} : {}}></i></td>
-                <td><i className="bi bi-check-lg linkedin" style={sunday.linkedin ? {display: "flex"} : {}}></i></td>
+                <td>{monday.linkedin ? <i className="bi bi-check-lg linkedin"></i> : <></> }</td>
+                <td>{tuesday.linkedin ? <i className="bi bi-check-lg linkedin"></i> : <></> }</td>
+                <td>{wednesday.linkedin ? <i className="bi bi-check-lg linkedin"></i> : <></> }</td>
+                <td>{thursday.linkedin ? <i className="bi bi-check-lg linkedin"></i> : <></> }</td>
+                <td>{friday.linkedin ? <i className="bi bi-check-lg linkedin"></i> : <></> }</td>
+                <td>{saturday.linkedin ? <i className="bi bi-check-lg linkedin"></i> : <></> }</td>
+                <td>{sunday.linkedin ? <i className="bi bi-check-lg linkedin"></i> : <></> }</td>
             </tr>
             <tr>
                 <th>Renda Variável</th>
