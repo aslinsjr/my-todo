417bcf5a62d38c20be11984b02dc65a98267f9ba
Alexandre Lins <aslinsjr@gmail.com>
2025-01-20 10:32:43 -0300

    Update GoalsComponent.jsx

--- src/components/GoalsComponent.jsx
+++ src/components/GoalsComponent.jsx
@@ -2,6 +2,8 @@ import './GoalsComponent.css'
 
 const GoalsComponent = () => {
 
+    console.log(JSON.parse(localStorage.getItem("weekGoals")))
+
     return (
         <div className="goals-container">
             {localStorage.getItem("weekGoals") ? JSON.parse(localStorage.getItem("weekGoals")).map((task) => {
