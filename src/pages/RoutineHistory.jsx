b6978f2f12221d121ad3f75ccc87aed07414c8b4
Alexandre Lins <aslinsjr@gmail.com>
2025-01-20 19:45:39 -0300

    V2 - Styles Updates

--- src/pages/RoutineHistory.jsx
+++ src/pages/RoutineHistory.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import './RoutinrHistory.css'
 
 const RoutineHistory = () => {
+
+  const dayArray = ["01", "02", "03", "04", "05", "06", "07"]
+
   return (
-    <div className='container' style={{alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
-      <h1>Em desenvolvimento</h1>
-      <p>Aguarde...</p>
+    <div className='container routine-history'>
+      <table>
+        <tbody>
+          <tr>
+            <th>Semana:</th>
+            <th>Segunda</th>
+            <th>Terça</th>
+            <th>Quarta</th>
+            <th>Quinta</th>
+            <th>Sexta</th>
+            <th>Sábado</th>
+            <th>Domingo</th>
+          </tr>
+          {localStorage.getItem("taskArray") ? JSON.parse(localStorage.getItem("taskArray")).map((task) => {
+            return (
+              <tr>
+                <th>{task.name}</th>
+                {dayArray.map((day) => {
+
+                  if (task.type !== "valor") {
+                    return (
+                      <td >{localStorage.getItem((task.name).toLowerCase().split(" ").join("") + day)}</td>
+                    )
+                  } else {
+                    return (
+                      <td>{localStorage.getItem((task.name).toLowerCase().split(" ").join("") + day) ? "R$ " + localStorage.getItem((task.name).toLowerCase().split(" ").join("") + day) : ""}</td>
+                    )
+                  }
+                })}
+              </tr>
+            )
+          }) : <></>}
+        </tbody>
+      </table>
     </div>
   )
 }
