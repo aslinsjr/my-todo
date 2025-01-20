7a7ecd2c271483bde8e7c5b75137567015b50ebc
Alexandre Lins <aslinsjr@gmail.com>
2025-01-20 10:52:04 -0300

    Update RoutineHistory.jsx

--- src/pages/RoutineHistory.jsx
+++ src/pages/RoutineHistory.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const RoutineHistory = () => {
   return (
-    <div className='container'>RoutineHistory</div>
+    <div className='container' style={{alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
+      <h1>Em desenvolvimento</h1>
+      <p>Aguarde...</p>
+    </div>
   )
 }
 
